ae364d9d6199c88848240bd9d35730fb19236e07
zac6ix <nanaquameallday@gmail.com>
2017-10-14 20:42:15 +0000

    use "brunch $codename" for build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd ../../../
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 ./device/Infinix/X601_LTE/patches/install.sh
 . build/envsetup.sh
-lunch lineage_X601_LTE-userdebug && make bacon -j10
+lunch lineage_X601_LTE-userdebug && brunch X601_LTE
